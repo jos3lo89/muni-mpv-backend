f2006850f03d3038675adb4e6d743b4ab3b8cb81
jos3lo89 <ganbaru743@gmail.com>
2025-11-25 11:23:48 -0500

    feat: Introduce Prisma ORM with initial schema

---
+++ src/core/prisma/prisma.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor(private readonly config: ConfigService) {
+    const dbUrl = config.get<string>('DATABASE_URL');
+
+    const adapter = new PrismaPg({
+      connectionString: dbUrl,
+    });
+
+    super({
+      adapter,
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
